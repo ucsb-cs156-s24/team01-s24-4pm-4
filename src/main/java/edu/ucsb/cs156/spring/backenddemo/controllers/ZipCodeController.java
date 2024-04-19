ef27f6db7d635fdc575a48a7ef1d7bf90646491d
Ridhit Garg <ridhitgarg@ucsb.edu>
2024-04-18 18:07:21 -0700

    rg - zipcode controller and tests implemented

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/ZipCodeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/ZipCodeController.java
@@ -2,8 +2,38 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.ZipCodeQueryService;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+@Tag(name="ZipCode info from http://www.zippopotam.us/")
+@Slf4j
 @RestController
+@RequestMapping("/api/zipcode")
 public class ZipCodeController {
-    
+    @Autowired
+    ZipCodeQueryService zipcodeQueryService;
+
+    @Operation(summary = "Get information about a us zipcode")
+    @GetMapping("/get")
+    public ResponseEntity<String> getZipCodes(
+        @Parameter(name="zipcode", description="us zipcode", example="93102") @RequestParam String zipcode
+    ) throws JsonProcessingException {
+        // log.info("getEarthquakes: distance={} minMag={}", distance, minMag);
+        String result = zipcodeQueryService.getJSON(zipcode);
+        return ResponseEntity.ok().body(result);
+    }
 }
