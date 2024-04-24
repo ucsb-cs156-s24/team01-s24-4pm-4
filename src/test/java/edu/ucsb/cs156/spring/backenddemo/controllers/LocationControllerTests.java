23ace8fe1437c2683454afc37430047934e5ba0a
emrecikisir <emrecikisir@ucsb.edu>
2024-04-24 15:05:26 -0700

    Implemented controller and tests

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
@@ -5,14 +5,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.MvcResult;
 
 import edu.ucsb.cs156.spring.backenddemo.services.LocationQueryService;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(LocationController.class)
 public class LocationControllerTests {
@@ -30,12 +32,11 @@ public class LocationControllerTests {
 
         when(locationQueryService.getJSON(any(String.class))).thenReturn(expectedJson);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/locations/get")
+        mockMvc.perform(get("/api/locations/get")
                 .param("location", location)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(expectedJson));
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedJson));
     }
 
     @org.junit.jupiter.api.Test
@@ -43,11 +44,10 @@ public class LocationControllerTests {
         String location = "Santa Barbara";
         when(locationQueryService.getJSON(any(String.class))).thenThrow(new RuntimeException("Service failure"));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/locations/get")
+        mockMvc.perform(get("/api/locations/get")
                 .param("location", location)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(MockMvcResultMatchers.content().string("{\"error\":\"Service failure\"}"));
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().string("{\"error\":\"Service failure\"}"));
     }
 }
