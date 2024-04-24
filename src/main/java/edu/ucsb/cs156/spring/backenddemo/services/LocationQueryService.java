43ac3338586cb383e521cc5bbed72d95be2094a7
emrecikisir <emrecikisir@ucsb.edu>
2024-04-24 14:21:27 -0700

    Implemented service and tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,29 +1,42 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.*;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import lombok.extern.slf4j.Slf4j;
+import java.util.Collections;
 
-
+@Slf4j
 @Service
 public class LocationQueryService {
 
     private final RestTemplate restTemplate;
 
     public LocationQueryService(RestTemplateBuilder restTemplateBuilder) {
-        restTemplate = restTemplateBuilder.build();
+        this.restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
-
+    private static final String ENDPOINT = "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
 
     public String getJSON(String location) throws HttpClientErrorException {
-    UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(ENDPOINT)
-                                                            .queryParam("q", location)
-                                                            .queryParam("format", "jsonv2");
-    return restTemplate.getForObject(uriBuilder.toUriString(), String.class);
+        log.info("Fetching location data for: {}", location);
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+            ENDPOINT,
+            HttpMethod.GET,
+            entity,
+            String.class,
+            Collections.singletonMap("location", location)
+        );
+
+        return response.getBody();
     }
 }
